0f74a3c1f6f1c8dbfd537b218ff663d3c51f0e4f
Benjamin Chan <chanb@ohsu.edu>
2017-02-09 10:40:45 -0800

    Update cart2sph function to accept data frames

--- lib/cart2sph.R
+++ lib/cart2sph.R
@@ -1,12 +1,12 @@
-cart2sph <- function (x, y, z, x0 = 0, y0 = 0, z0 = 0, units = "rad") {
+cart2sph <- function (v, v0 = data.frame(x= 0, y = 0, z = 0), units = "rad") {
   # See http://mathworld.wolfram.com/SphericalCoordinates.html and https://en.wikipedia.org/wiki/Spherical_coordinate_system
   # Use ISO convention
   # rho = radial distance
   # theta = polar angle (inclination) bounded between [0, pi] or [0, 180]; default units is radians
   # phi = azimuthal angle bounded between [0, 2 * pi] or [0, 360]; default units is radians
-  x <- x - x0
-  y <- y - y0
-  z <- z - z0
+  x <- v[, 1] - v0[, 1]
+  y <- v[, 2] - v0[, 2]
+  z <- v[, 3] - v0[, 3]
   rho <- sqrt(x^2 + y^2 + z^2)
   theta <- acos(z / rho)
   phi <- atan2(y, x) + pi
